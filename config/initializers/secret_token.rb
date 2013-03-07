7098a6364ebaa3029890606e035171124dad6434
Tuan Nguyen <nguyen.tt.1978@gmail.com>
2013-03-07 23:25:55 +0100

    Regeneration using rails 3

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '0c968218836befd615976c17e62602d4f53d525850d64937d6af91136e66b3287459bd82c32a48914c4445be98e27ca3c11877840be6451591847582c54b1d55';
+DataSavant::Application.config.secret_token = '23547e8e8b50b7a9a31eba7773d5ab54dd397ec81c41fe5248eb8da8f2d4ce045a97c05779f4e0476cf776d10e2b4727cfe7248911f50dfeafc1726a0ef74ed4'
